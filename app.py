716edaaafd0ba28975dcda5feffa6f345367b139
Ariffaji <arifnauval1530@gmail.com>
2025-09-03 10:31:39 +0700

    fix deployment config for Railway

--- app.py
+++ app.py
@@ -694,3 +694,10 @@ if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     app.run(debug=True)
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
